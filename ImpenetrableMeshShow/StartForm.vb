bd2ee2131e7f34bd56d7b2ba8b9fabcdbe9f58b3
NorvezskayaSemga <simak_64@mail.ru>
2021-03-19 01:18:40 +1000

    Added exp->level and level->exp test functions

--- ImpenetrableMeshShow/StartForm.vb
+++ ImpenetrableMeshShow/StartForm.vb
@@ -337,3 +337,67 @@ Friend Class StartForm
     End Sub
 
 End Class
+
+Public Class D2Scripts
+
+    Public Shared Function ExpToLevelSlow(ByRef expToConvertion As Integer, ByRef baseLevel As Integer, ByRef dynLevel As Integer, _
+                                          ByRef baseExp As Integer, ByRef expNext1 As Integer, ByRef expNext2 As Integer) As Integer
+        Dim e As Integer = expToConvertion
+        Dim bar As Integer = baseExp
+        Dim level As Integer = baseLevel
+        Do While e >= bar
+            e -= bar
+            level += 1
+            If level <= dynLevel Then
+                bar += expNext1
+            Else
+                bar += expNext2
+            End If
+        Loop
+        Return level
+    End Function
+    Public Shared Function LevelToExpFast(ByRef baseLevel As Integer, ByRef currentLevel As Integer, ByRef dynLevel As Integer, _
+                                          ByRef baseExp As Integer, ByRef expNext1 As Integer, ByRef expNext2 As Integer) As Integer
+        Dim e1, e2 As Integer
+        Dim overlevel1, overlevel2 As Integer
+
+        If currentLevel > dynLevel Then
+            overlevel1 = dynLevel - baseLevel
+            overlevel2 = currentLevel - dynLevel
+        ElseIf currentLevel > baseLevel Then
+            overlevel1 = currentLevel - baseLevel
+            overlevel2 = 0
+        Else
+            overlevel1 = 0
+            overlevel2 = 0
+        End If
+
+        If overlevel1 > 0 Then
+            e1 = baseExp * overlevel1 + (overlevel1 - 1) * overlevel1 * expNext1 * 0.5
+        Else
+            e1 = 0
+        End If
+        If overlevel2 > 0 Then
+            Dim dynExp As Integer = baseExp + overlevel1 * expNext1
+            e2 = dynExp * overlevel2 + (overlevel2 - 1) * overlevel2 * expNext2 * 0.5
+        Else
+            e2 = 0
+        End If
+        Return e1 + e2
+    End Function
+    Public Shared Function LevelToExpSlow(ByRef baseLevel As Integer, ByRef currentLevel As Integer, ByRef dynLevel As Integer, _
+                                          ByRef baseExp As Integer, ByRef expNext1 As Integer, ByRef expNext2 As Integer) As Integer
+        Dim e As Integer = 0
+        Dim bar As Integer = baseExp
+        For i As Integer = baseLevel To currentLevel - 1 Step 1
+            e += bar
+            If i < dynLevel Then
+                bar += expNext1
+            Else
+                bar += expNext2
+            End If
+        Next i
+        Return e
+    End Function
+
+End Class
